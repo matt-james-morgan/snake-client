4177907486ed67b246322f45570081b6608eba84
Matt Morgan <matt.james.morgan97@gmail.com>
2023-11-13 16:24:45 +0000

    connecting to server adn displaying messages

---
+++ play.js
@@ -0,0 +1,19 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",// IP address here,
+    port: "50541"// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("data", ()=>{
+  console.log("you ded cause you idled");
+});
+  return conn;
+};
+
+console.log("Connecting ...");
+connect()
